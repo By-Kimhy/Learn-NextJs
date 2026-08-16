6416984f80173ad660e342531f6e14d634fdbc52
Kim hy <kimhyby99@gmail.com>
2026-08-17 00:56:32 +0700

    not found page

--- src/app/products/[productId]/review/[reviewId]/page.tsx
+++ src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -1,13 +1,19 @@
+import { notFound } from "next/navigation";
+
 export default async function ReviewDetails({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
-  const productId = (await params).productId;
-  const reviewId = (await params).reviewId;
+  const { productId, reviewId } = await params;
+  if (parseInt(reviewId) > 1000) {
+    notFound();
+  }
   return (
     <>
-      <h1>Review {reviewId} for product {productId}</h1>
+      <h1>
+        Review {reviewId} for product {productId}
+      </h1>
     </>
   );
 }
