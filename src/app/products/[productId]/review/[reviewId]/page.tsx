5f3373a744df791c984c67b59c5540077e816e64
ByKimhy <kimhyby99@gmail.com>
2026-08-23 13:44:28 +0700

    error handling

--- src/app/products/[productId]/review/[reviewId]/page.tsx
+++ src/app/products/[productId]/review/[reviewId]/page.tsx
@@ -1,14 +1,21 @@
-import { notFound,redirect } from "next/navigation";
+import { notFound} from "next/navigation";
 
-export default async function ReviewDetails({
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
+
+export default async function ProductReview({
   params,
 }: {
   params: { productId: string; reviewId: string };
 }) {
+  const random= getRandomInt(2);
+  if (random === 1) {
+    throw new Error("Error Loading review");
+  }
   const { productId, reviewId } = await params;
   if (parseInt(reviewId) > 1000) {
-    // notFound();
-    redirect("/products");
+    notFound();
   }
   return (
     <>
